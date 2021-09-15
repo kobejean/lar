ac51191fb4daca91c8a55c266deaa4d93ce61db5
Jean Atsumi Flaherty <kobejean@me.com>
2021-09-15 23:40:57 +0900

    Use `FetchContent`

---
+++ apps/bundle_adjustment.cpp
@@ -0,0 +1,73 @@
+// g2o - General Graph Optimization
+// Copyright (C) 2011 H. Strasdat
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <stdint.h>
+
+#include <iostream>
+#include <unordered_set>
+
+#include <nlohmann/json.hpp>
+
+#include "g2o/core/optimization_algorithm_factory.h"
+#include "g2o/core/robust_kernel_impl.h"
+#include "g2o/core/sparse_optimizer.h"
+#include "g2o/solvers/structure_only/structure_only_solver.h"
+#include "g2o/stuff/sampler.h"
+#include "g2o/types/sba/types_six_dof_expmap.h"
+#include "g2o/types/sba/types_six_dof_expmap.h"
+
+#if defined G2O_HAVE_CHOLMOD
+G2O_USE_OPTIMIZATION_LIBRARY(cholmod);
+#else
+G2O_USE_OPTIMIZATION_LIBRARY(eigen);
+#endif
+
+G2O_USE_OPTIMIZATION_LIBRARY(dense);
+
+using namespace Eigen;
+using namespace std;
+using json = nlohmann::json;
+
+int main(int argc, const char* argv[]){
+
+  // instead, you could also write (which looks very similar to the JSON above)
+  json j2 = {
+    {"pi", 3.141},
+    {"happy", true},
+    {"name", "Niels"},
+    {"nothing", nullptr},
+    {"answer", {
+      {"everything", 42}
+    }},
+    {"list", {1, 0, 2}},
+    {"object", {
+      {"currency", "USD"},
+      {"value", 42.99}
+    }}
+  };
+
+  std::cout << j2.dump(4) << std::endl;
+}
--- build.sh
+++ build.sh
@@ -1,18 +1,1 @@
-# Build G2O
-cd third_party/g2o
-echo "Configuring and building Thirdparty/g2o ..."
-mkdir build
-cd build
-mkdir install
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./install
-make -j
-make install
-cd ../../../
-
-# Build ba_demo
-echo "Building ba_demo ..."
-mkdir build
-cd build
-cmake ..
-make -j ba_demo
-cd ../
+make all
