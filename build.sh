087f8e346561dd9863f5f315ccaa1d566fb2c121
Jean Flaherty <kobejean@me.com>
2021-12-15 21:42:32 +0900

    Run `build.sh` if thirdparty include files are not installed

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
-mkdir tmp
-cd tmp
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+INCLUDE_DIR="$SCRIPT_DIR/thirdparty/include"
+TMP_DIR="$SCRIPT_DIR/tmp"
+TMP_INSTALL_DIR="$TMP_DIR/install"
 
-mkdir install
-cmake ../thirdparty/eigen3 -DCMAKE_INSTALL_PREFIX=./install
+mkdir $INCLUDE_DIR
+mkdir $TMP_DIR
+cd $TMP_DIR
+
+mkdir $TMP_INSTALL_DIR
+cmake "$SCRIPT_DIR/thirdparty/eigen3" -DCMAKE_INSTALL_PREFIX=$TMP_INSTALL_DIR
 make install -j 8
-cp -R ./install/include/eigen3 ../include
+EIGEN_STATUS=$?
+cp -R "$TMP_INSTALL_DIR/include/eigen3" $INCLUDE_DIR
 rm -r *
 
-mkdir install
-cmake ../thirdparty/opencv -DCMAKE_INSTALL_PREFIX=./install \
+mkdir $TMP_INSTALL_DIR
+cmake "$SCRIPT_DIR/thirdparty/opencv" -DCMAKE_INSTALL_PREFIX=$TMP_INSTALL_DIR \
     -DBUILD_LIST="calib3d,features2d,imgcodecs" \
     -DBUILD_SHARED_LIBS=OFF \
     -DBUILD_DOCS=OFF \
@@ -34,14 +41,22 @@ cmake ../thirdparty/opencv -DCMAKE_INSTALL_PREFIX=./install \
     -DWITH_IMGCODEC_PXM=OFF \
     -DWITH_IMGCODEC_PFM=ON
 make install -j 8
-cp -R ./install/include/opencv4/opencv2 ../include
+OPENCV_STATUS=$?
+cp -R "$TMP_INSTALL_DIR/include/opencv4/opencv2" $INCLUDE_DIR
 rm -r *
 
 mkdir install
-cmake ../thirdparty/g2o -DCMAKE_INSTALL_PREFIX=./install -DEIGEN3_INCLUDE_DIR=$(cd "../include/eigen3"; pwd) -DG2O_USE_VENDORED_CERES=ON
+cmake "$SCRIPT_DIR/thirdparty/g2o" -DCMAKE_INSTALL_PREFIX=$TMP_INSTALL_DIR -DEIGEN3_INCLUDE_DIR="$INCLUDE_DIR/eigen3" -DG2O_USE_VENDORED_CERES=ON
 make install -j 8
-cp -R ./install/include/g2o ../include
+G2O_STATUS=$?
+cp -R "$TMP_INSTALL_DIR/include/g2o" $INCLUDE_DIR
 rm -r *
 
-cd ..
-make all
+if [ $EIGEN_STATUS -eq 0 ] && [ $OPENCV_STATUS -eq 0 ] && [ $G2O_STATUS -eq 0 ]
+then
+    echo "Third Party Includes Successfully Generated"
+    rm -rf $TMP_DIR
+else
+    cd $SCRIPT_DIR
+    rm -rf $INCLUDE_DIR $TMP_DIR
+fi
