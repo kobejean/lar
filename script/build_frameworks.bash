93177e2ef9f8cf048b742921f0f5eac840fb680c
Jean Flaherty <kobejean@me.com>
2022-01-06 22:47:24 +0900

    Fix artifacts script

--- script/build_frameworks.bash
+++ script/build_frameworks.bash
@@ -8,5 +8,5 @@ mkdir build/frameworks
 ./script/build_geoar_framework.bash
 
 # Uncomment if opencv needs to be rebuilt
-# Otherwise just download the framework from here:
+# Otherwise just download the framework from here: https://github.com/kobejean/GeoARCore/releases/download/v1.0.0-alpha.0/opencv2.xcframework.zip
 # ./script/build_opencv_framework.bash
