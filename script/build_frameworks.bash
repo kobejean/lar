0c05bc9f398a0645941091f1badbf8d6ea5e3e63
Jean Flaherty <kobejean@me.com>
2022-01-06 21:33:45 +0900

    Disable opencv framework build

--- script/build_frameworks.bash
+++ script/build_frameworks.bash
@@ -3,6 +3,10 @@ if [[ ! -d build ]]; then
     make fast
 fi
 mkdir build/frameworks
+
 ./script/build_g2o_framework.bash
 ./script/build_geoar_framework.bash
-./script/build_opencv_framework.bash
+
+# Uncomment if opencv needs to be rebuilt
+# Otherwise just download the framework from here:
+# ./script/build_opencv_framework.bash
