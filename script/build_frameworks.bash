c011ed52a199b6c827fb70e494f22a08718b590e
Jean Flaherty <kobejean@me.com>
2022-01-27 17:19:49 +0900

    Update build_frameworks.bash

--- script/build_frameworks.bash
+++ script/build_frameworks.bash
@@ -4,8 +4,8 @@ if [[ ! -d build ]]; then
 fi
 mkdir build/frameworks
 
-./script/build_g2o_framework.bash
 ./script/build_geoar_framework.bash
+./script/build_g2o_framework.bash
 
 # Uncomment if opencv needs to be rebuilt
 # Otherwise just download the framework from here: https://github.com/kobejean/GeoARCore/releases/download/v1.0.0-alpha.0/opencv2.xcframework.zip
