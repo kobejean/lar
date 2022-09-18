96a0fdb2e8da944193bea503908c47ab21c6f772
Jean Flaherty <kobejean@me.com>
2022-09-18 11:42:18 +0900

    Try

---
+++ include/lar/core/data_structures/unordered_array.h
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <array>
+
+namespace lar {
+
+  template <class T, std::size_t N>
+  struct unordered_array {
+      std::array<T, N> _data;
+      std::size_t _size;
+    public:
+      unordered_array() : _size(0) {}
+
+      using iterator = typename std::array<T, N>::iterator;
+      using const_iterator = typename std::array<T, N>::const_iterator;
+
+      T& operator[](std::size_t index) { return _data[index]; }
+      const T& operator[](std::size_t index) const { return _data[index]; }
+
+
+      void push_back(T value) { _data[_size++] = value; }
+
+      void pop_front() { _data[0] = _data[_size--]; }
+      void pop_front(std::size_t k) {
+        auto end = _data.begin() + _size;
+        _size -= k;
+        std::size_t offset = std::max(k, _size);
+        std::copy(_data.begin() + offset, end, _data.begin());
+        // for (size_t i = 0; i < _size; ++i) {
+        //   _data[i] = _data[i + k];
+        // }
+      }
+      
+      void pop_back() { _size--; }
+      void pop_back(std::size_t k) { _size -= k; }
+
+      void erase(std::size_t index) { _data[index] = _data[--_size]; }
+
+      iterator clear() { _size = 0; }
+
+      std::size_t size() const { return _size; }
+
+      iterator begin() noexcept { return _data.begin(); }
+      constexpr const_iterator begin() const noexcept { return _data.begin(); }
+
+      iterator end() noexcept { return _data.begin() + _size; }
+      constexpr const_iterator end() const noexcept { return _data.begin() + _size; }
+  };
+  
+}
---
+++ include/lar/core/data_structures/unordered_vector.h
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <array>
+
+namespace lar {
+
+  template <class T>
+  struct unordered_vector {
+      std::vector<T> _data;
+    public:
+      using iterator = typename std::vector<T>::iterator;
+      using const_iterator = typename std::vector<T>::const_iterator;
+
+      T& operator[](std::size_t index) { return _data[index]; }
+      const T& operator[](std::size_t index) const { return _data[index]; }
+
+
+      void push_back(T value) { _data.push_back(value); }
+
+      void pop_front() { _data[0] = _data.back(); pop_back(); }
+      void pop_front(std::size_t k) {
+        auto offset = _data.begin() + std::max(k, size() - k);
+        // for (size_t i = 0; i < std::min(k, size() - k); i++) {
+        //   _data[i] = _data[i + offset];
+        // }
+        std::copy(offset, _data.end(), _data.begin());
+        pop_back(k);
+      }
+      
+      void pop_back() { _data.pop_back(); }
+      void pop_back(std::size_t k) { _data.resize(_data.size() - k); }
+
+      template< class InputIt >
+      iterator insert(const_iterator pos, InputIt first, InputIt last ) {
+        return _data.insert(pos, first, last);
+      }
+
+      void erase(std::size_t index) { _data[index] = _data.back(); pop_back(); }
+
+      iterator clear() { _data.clear(); }
+
+      std::size_t size() const { return _data.size(); }
+
+      iterator begin() noexcept { return _data.begin(); }
+      constexpr const_iterator begin() const noexcept { return _data.begin(); }
+
+      iterator end() noexcept { return _data.end(); }
+      constexpr const_iterator end() const noexcept { return _data.end(); }
+  };
+  
+}
